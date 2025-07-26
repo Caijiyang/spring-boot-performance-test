37efd1ec13a29ff40568411d1b4297e9a105546e
FelixCJY <3049606735@qq.com>
2025-07-26 19:32:48 +0800

    feat:加入 knife4j

--- web-server-1/src/main/java/org/felixcjy/config/OpenApiConfig.java
+++ web-server-1/src/main/java/org/felixcjy/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package org.felixcjy.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,8 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("多模块 API 文档")
-                        .version("1.0")
-                        .description("Spring Boot 2.7.18 + OpenAPI 3"));
+                        .contact(new Contact().name("蔡济阳(Felix)"))
+                        .description("本接口文档由后端团队（我）倾情奉献，使用前请深呼吸三秒，准备迎接秩序与逻辑的盛宴。")
+                        .version("1.0"));
     }
 }
